8922fd8ffaf90fc5e0ad0ebd59e3d8407d73e503
Dayanaalvespereira <125765135+Dayanaalvespereira@users.noreply.github.com>
2023-05-18 11:18:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+(
+ async function(){
+    new  Chart(
+       document.getElementById("grafico1"),
+       {
+          type: 'bar',
+          data: {
+            labels : ["Português", "Matemática"],
+            datasets: [
+                {
+                    label: "Média de Notas",
+                    data: [7.5, 10]
+                }
+            ]
+          }
+       }
+
+   )
+   
+
+}
+
+    )();
